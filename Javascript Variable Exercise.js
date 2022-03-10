b7f4da89816000dc69139286e35e96d6f3a2ea2a
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-03-10 12:34:53 -0500

    Javascript Exercise 1

---
+++ Javascript Variable Exercise.js
@@ -0,0 +1,16 @@
+function test() {
+    let a = "3";
+    let b = "8";
+
+    /***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+
+    let c = a;
+    a = b;
+    b = c;
+
+    /***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
